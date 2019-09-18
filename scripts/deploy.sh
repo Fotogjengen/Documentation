66dabf02bd9a68d420711ae419e7a56b72c1d57e
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2019-09-18 22:15:32 +0200

    :rocket: build and start docker continer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo "deploy"
-ssh -i ./deploy_key hilfling@$SERVER_IP_ADDRESS 'cd /var/www/hilfling-server && git pull'
 
+ssh -i ./deploy_key hilfling@$SERVER_IP_ADDRESS 'cd /var/www/hilfling-server && git pull && docker-compose up -d --build && echo "build success"'
